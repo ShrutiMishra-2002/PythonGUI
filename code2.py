b3cdd506c7baeeaaf3e4da635c77ff9ced7aa608
Shruti Mishra <shrutidmishra2002@gmail.com>
2021-11-29 19:14:59 +0530

    initial commit

---
+++ code2.py
@@ -0,0 +1,9 @@
+# Pycharm GUI
+from tkinter import *
+Root = Tk()
+Root.geometry("644x434")
+Root.minsize(733,435)
+Root.maxsize(1200,644)
+text1 = Label(text="Welcome to pycharm")
+text1.pack()
+Root.mainloop()
